192de8e393f698249a2e7fe595f5ddb7c80f934b
Lionel Surmont <lionel.surmont@gmail.com>
2020-06-30 20:36:30 +0200

    rails mr cocktail fix

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,6 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail, presence: true, uniqueness: { scope: [:ingredient],
-    message: "There should be a unique dose for a given cocktail/ingredient couple" }
+  validates :cocktail, uniqueness: { scope: [:ingredient] }#,
+    # message: "There should be a unique dose for a given cocktail/ingredient couple" }
 end
