c3f59468b087a4f0ddce7dd3938fd7149c22ddf8
Lionel Surmont <lionel.surmont@gmail.com>
2020-06-30 20:27:20 +0200

    rails mr cocktail

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,37 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:destroy]
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+  
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id].to_i)
+    
+    # CREATE NEW DOSE
+    @dose = Dose.new(dose_params)
+    # ASSIGN LINKED OBJECTS
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    # SAVE
+    @dose.save
+    # REDIRECT
+    redirect_to cocktail_path(@cocktail)
+  end
+  
+  def destroy
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Seeding Ingredients
+require 'json'
+require 'open-uri'
+
+url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+serialized_ingredients = open(url).read
+ingredients = JSON.parse(serialized_ingredients)
+# p ingredients
+
+p "Cleaning the ingredients database"
+if Rails.env.development?
+  p "Rails is in development environment"
+  Ingredient.destroy_all 
+end
+p "Seeding..."
+i = 0
+100.times do
+  ingredient = Ingredient.create(
+    name: ingredients["drinks"][i]["strIngredient1"]
+    )
+  p "Ingredient: #{ingredient.name} is created"
+  i += 1
+end
+
+p "Done seeding!"
+# Seeding ...
